2826e9b3ef4fbca038cd7759bc66b786790d849b
binhfnguyeen <nguyendzaii16112004@gmail.com>
2023-08-08 11:57:09 +0700

    8/8/2023

--- DangNhap-DangKy/kiemtra.js
+++ DangNhap-DangKy/kiemtra.js
@@ -1,46 +1,20 @@
 function login() {
-  var user = document.getElementById("login-username").value;
-  var pass = document.getElementById("login-password").value;
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
 
-  const jsonData = localStorage.getItem('userData');
+    fetch('data.json')
+        .then(response => response.json())
+        .then(data => {
+            const user = data.find(item => item.username === username && item.password === password);
 
-  if (jsonData) {
-    const userData = JSON.parse(jsonData);
-
-    if (user === userData.user && pass === userData.pass) {
-      alert('Đăng nhập thành công!');
-      return true;
-    } else {
-      alert('Tên đăng nhập hoặc mật khẩu không đúng.');
-      return false;
-    }
-  } else {
-    alert('Không tìm thấy thông tin người dùng.');
-    return false;
-  }
+            if (user) {
+                alert("Đăng nhập thành công!");
+                window.location.href = "your_target_page.html";
+                return false;
+            } else {
+                alert("Tên đăng nhập hoặc mật khẩu không đúng. Vui lòng thử lại.");
+                return false;
+            }
+        })
+        .catch(error => console.error('Error reading JSON file:', error));
 }
-
-function validateForm() {
-  var user = document.getElementById("username").value;
-  var pass = document.getElementById("password").value;
-  var repass = document.getElementById("password-repeat").value;
-
-  if (user === '' || pass === '' || repass === '') {
-    alert("Vui lòng nhập đầy đủ thông tin!");
-    return false;
-  }
-  if (pass !== repass) {
-    alert("Mật khẩu nhập lại không đúng!");
-    return false;
-  }
-
-  const userData = {
-    user: user,
-    pass: pass
-  };
-  localStorage.setItem('userData', JSON.stringify(userData));
-  console.log('Đã lưu thông tin người dùng vào Local Storage.');
-  alert("Đăng ký thành công!")
-  return true;
-}
-
